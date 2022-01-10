fa4ec62c966df0e29e7d5093da0ede64fb696b38
junior <mjuniorramos112@gmail.com>
2022-01-10 15:06:52 -0300

    JavaScript Toggle Menu/Section About and Services Initial

---
+++ main.js
@@ -0,0 +1,18 @@
+/* abrir e fechar menu quando clicar nos icones */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+/* abrir e fechar menu quando clicar nos links */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
